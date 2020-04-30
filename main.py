80f987e2138a81a1fb2419e3a4e83bbd474cdff0
Ryland Donovan <RrylandD@gmail.com>
2020-04-30 18:07:56 -0400

    okay

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-limport os
+import os
 import os.path
 
 import discord
 from dotenv import load_dotenv
 
 from discord.ext import commands
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('./service-account.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -18,16 +28,13 @@ async def on_ready():
 @bot.command(name='sitlist')
 async def sit_list(ctx):
 
-	file_exists = os.path.isfile("sitlist.txt")
-	if file_exists:
-		response = 'Ryland has been sitted by:\n'
-		with open("sitlist.txt", "r") as txt_file:
-			for name in txt_file:
-				response+= '\t- ' + name
+	#Base response
+	response = 'Ryland has been sitted by:\n'
 
-	else:
-		open("sitlist.txt", "a+")
-		response = 'Ryland hasn\'t been sitted\n'
+	#Get all docs and append names
+	docs = db.collection(u'sitlist').stream()
+	for doc in docs:
+		response += '\t- '+doc.to_dict()["Name"]+'\n'
     
 	await ctx.send(response)
 
@@ -35,8 +42,11 @@ async def sit_list(ctx):
 async def sit(ctx, *mob_name):
 
 	response = 'Ryland got sitted by ' + ' '.join(mob_name)
-	with open("sitlist.txt", "a+") as txt_file:
-		txt_file.write(' '.join(mob_name)+'\n')
+
+	data = {u'Name':' '.join(mob_name)}
+
+	db.collection(u'sitlist').document(' '.join(mob_name)).set(data)
+
 	await ctx.send(response)
 
 bot.run(TOKEN)
